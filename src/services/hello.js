d91d761808618e34554192b077d44e26593f96ce
Gany <itisgany@gmail.com>
2025-12-28 21:07:08 -0500

    env var update in hello.ts

--- src/services/hello.js
+++ src/services/hello.js
@@ -3,7 +3,8 @@ exports.main = async function(event, context) {
         statusCode: 200,
         body: JSON.stringify({
             message: 'Hello from Ganesh Natarjan lambda!, Time now is',
-            currentDateTime: new Date().toISOString()
+            currentDateTime: new Date().toISOString(),
+            message: `Hello! I will read from ${process.env.SPACES_TABLE_NAME} table`
         })
     }
 }
