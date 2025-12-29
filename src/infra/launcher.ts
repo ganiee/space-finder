df8cf1a56ec0049fd2084c76c9e1ecb920b5c254
Gany <itisgany@gmail.com>
2025-12-29 15:46:14 -0500

    spaces lambda GET and POST

--- src/infra/launcher.ts
+++ src/infra/launcher.ts
@@ -12,6 +12,6 @@ const lambdaStack = new LambdaStack(app, 'LambdaStack', {
     spacesTable: datastack.spacesTable
 });
 new ApiStack(app, 'ApiStack', {
-    helloLambdaIntegration: lambdaStack.helloLambdaIntegration
+    spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration
 })
 
