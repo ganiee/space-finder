e8966c9f2d6c4ecc0ba9db2a9f0d9d2bc2b0f050
Gany <itisgany@gmail.com>
2025-12-28 21:11:01 -0500

    lambda output change

--- src/services/hello.js
+++ src/services/hello.js
@@ -2,9 +2,8 @@ exports.main = async function(event, context) {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello from Ganesh Natarjan lambda!, Time now is',
             currentDateTime: new Date().toISOString(),
-            message: `Hello! I will read from ${process.env.SPACES_TABLE_NAME} table`
+            message: `Hello Gany ! I will read from ${process.env.SPACES_TABLE_NAME} table`
         })
     }
 }
