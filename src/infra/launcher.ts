eeb2271ae57a9b789eee20f41d54567d825169c7
Gany <itisgany@gmail.com>
2025-12-28 20:16:25 -0500

    api gw changes

--- src/infra/launcher.ts
+++ src/infra/launcher.ts
@@ -1,6 +1,7 @@
 import { App } from 'aws-cdk-lib';
 import { DataStack } from './stacks/DataStack';
 import { LambdaStack } from './stacks/LambdaStack';
+import { ApiStack } from './stacks/ApiStack';
 
 console.log('launcher running');
     
@@ -8,5 +9,7 @@ const app = new App();
 
 const datastack = new DataStack(app, 'DataStack');
 const lambdaStack = new LambdaStack(app, 'LambdaStack');
-
+new ApiStack(app, 'ApiStack', {
+    helloLambdaIntegration: lambdaStack.helloLambdaIntegration
+})
 
