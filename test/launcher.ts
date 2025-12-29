dfed7e4ddeee9a4b5814e47f5c85a426e1c18a6d
Gany <itisgany@gmail.com>
2025-12-29 18:55:22 -0500

    handler launch fix

--- test/launcher.ts
+++ test/launcher.ts
@@ -1,13 +1,18 @@
 import { handler } from "../src/services/spaces/handler";
 
 
-process.env.AWS_REGION = 'us-east-2';
-process.env.TABLE_NAME = 'SpaceTable-064b1b71a08f';
+// handler({} as any, {} as any).then(response => {
+//     console.log("Lambda response:", response);
+// }); 
 
+process.env.TABLE_NAME = 'SpaceTable-064b1b71a08f';
+process.env.AWS_REGION = 'us-east-2';
 
 handler({
     httpMethod: 'POST',
     body: JSON.stringify({
-         location: 'new jersey'
+         location: 'old bridge'
     })
-} as any, {} as any);
+} as any, {} as any).then(response => {
+   console.log("Lambda response:", response);
+}); 
