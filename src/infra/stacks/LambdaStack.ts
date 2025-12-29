d4b0b5a18123d32de21b139fac49418c55cfea16
Gany <itisgany@gmail.com>
2025-12-28 20:39:55 -0500

    dynamo db changes

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,19 +1,28 @@
 import { Stack, StackProps }from 'aws-cdk-lib';
+import { ITable } from 'aws-cdk-lib/aws-dynamodb';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Runtime, Function as LambdaFunction } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+
+interface LambdaStackProps extends StackProps {
+    spacesTable: ITable
+}
+
 
 export class LambdaStack extends Stack {
 
     public readonly helloLambdaIntegration: LambdaIntegration
 
-    constructor(scope: Construct, id: string, props?: StackProps) {
+    constructor(scope: Construct, id: string, props: LambdaStackProps) {
         super(scope, id, props)
 
         const helloLambda = new LambdaFunction(this, 'HelloHandler', {
             runtime: Runtime.NODEJS_18_X,
             code: Code.fromAsset('src/services'),
-            handler: 'hello.main'
+            handler: 'hello.main',
+            environment: {
+                SPACES_TABLE_NAME: props.spacesTable.tableName
+            }
         });
 
 
