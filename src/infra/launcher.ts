60ccadf41e39bda6f479a28b35ada25cca72dfee
Gany <itisgany@gmail.com>
2025-12-28 19:10:38 -0500

    lambda stack commit

--- src/infra/launcher.ts
+++ src/infra/launcher.ts
@@ -1,11 +1,12 @@
 import { App } from 'aws-cdk-lib';
 import { DataStack } from './stacks/DataStack';
+import { LambdaStack } from './stacks/LambdaStack';
 
 console.log('launcher running');
     
 const app = new App();
 
 const datastack = new DataStack(app, 'DataStack');
-
+const lambdaStack = new LambdaStack(app, 'LambdaStack');
 
 
---
+++ src/infra/stacks/LambdaStack.ts
@@ -0,0 +1,17 @@
+import { Stack, StackProps }from 'aws-cdk-lib';
+import { Code, Runtime, Function as LambdaFunction } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+
+export class LambdaStack extends Stack {
+
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props)
+
+        new LambdaFunction(this, 'HelloHandler', {
+            runtime: Runtime.NODEJS_18_X,
+            code: Code.fromAsset('src/services'),
+            handler: 'hello.main'
+        });
+
+    }
+}
