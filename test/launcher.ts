5dfcbd977d2ad7f3caa0dfede55768ef201f5bf5
Gany <itisgany@gmail.com>
2025-12-29 21:42:49 -0500

    ddb-scan changes

--- test/launcher.ts
+++ test/launcher.ts
@@ -8,11 +8,18 @@ import { handler } from "../src/services/spaces/handler";
 process.env.TABLE_NAME = 'SpaceTable-064b1b71a08f';
 process.env.AWS_REGION = 'us-east-2';
 
+// handler({
+//     httpMethod: 'POST',
+//     body: JSON.stringify({
+//          location: 'edison'
+//     })
+// } as any, {} as any).then(response => {
+//    console.log("Lambda response:", response);
+// }); 
+
 handler({
-    httpMethod: 'POST',
-    body: JSON.stringify({
-         location: 'edison'
-    })
+    httpMethod: 'GET',
+   
 } as any, {} as any).then(response => {
    console.log("Lambda response:", response);
 }); 
